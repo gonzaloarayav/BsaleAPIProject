f32510321a2e5b5c97de25b8ea172da5f0de3b64
gonzaloarayav <gonzaloarayav@hotmail.com>
2021-08-10 16:36:38 -0400

    BsaleAPI Django V1.3

--- BsaleAPIProject/settings.py
+++ BsaleAPIProject/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ROOT_PATH = os.path.dirname(__file__)
+
+STATIC_ROOT = os.path.join(ROOT_PATH, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
